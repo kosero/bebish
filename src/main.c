724a7f7e2214ec167a1e2a4e791a8017abe7f63f
kosero <kosero@tuta.io>
2026-09-16 06:24:10 +0300

    feat: format shell prompt with user, hostname, and home path shortcut

--- src/main.c
+++ src/main.c
@@ -166,10 +166,27 @@ static void execute_args(char *args[]) {
   }
 }
 
+static void get_system_hostname(char *buffer, size_t len) {
+  if (gethostname(buffer, len)) {
+    (void)snprintf(buffer, len, "unknown");
+  }
+}
+
 static void print_prompt() {
   char cwd[1024];
+  char hostname[256];
+  get_system_hostname(hostname, sizeof(hostname));
+
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-    printf("%s $ ", cwd);
+    const char *prefix = get_env_var("HOME");
+    size_t prefix_len = (prefix != NULL) ? strlen(prefix) : 0;
+
+    if (prefix != NULL && strncmp(cwd, prefix, prefix_len) == 0) {
+      memmove(cwd, cwd + prefix_len, strlen(cwd + prefix_len) + 1);
+      printf("%s@%s ~%s$ ", get_env_var("USER"), hostname, cwd);
+    } else {
+      printf("%s@%s ~%s$ ", get_env_var("USER"), hostname, cwd);
+    }
   } else {
     perror("[bebish]: getcwd failed");
   }
